7e1b987f01caa272bcc3e2e8ff0e039afaf44872
catus felis <catus.f@gmail.com>
2022-10-26 01:51:41 +0000

    Add script to convert mdx files

---
+++ convert_mdx.sh
@@ -0,0 +1,4 @@
+pyglossary --remove-html-all './dict/Prodict EV Tech.mdx'     './dict/Prodict EV Tech.tab'
+pyglossary --remove-html-all './dict/Prodict EV business.mdx' './dict/Prodict EV business.tab'
+pyglossary --remove-html-all './dict/Prodict VE Tech.mdx'     './dict/Prodict VE Tech.tab'
+pyglossary --remove-html-all './dict/Prodict VE business.mdx' './dict/Prodict VE business.tab'
